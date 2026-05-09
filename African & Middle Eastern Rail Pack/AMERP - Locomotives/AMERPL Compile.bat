647f7effc737bd4ae6fe8804245ef66d492997da
Ragin1924 <zacm400@gmail.com>
2026-05-09 16:16:33 +1000

    new locos

---
+++ African & Middle Eastern Rail Pack/AMERP - Locomotives/AMERPL Compile.bat
@@ -0,0 +1,3 @@
+cd C:\Users\Zac\Desktop\Ragin's Sets\AMERP - Locomotives
+nmlc -c --grf AMERPL.grf AMERPL.nml
+pause
---
+++ African & Middle Eastern Rail Pack/AMERP - Locomotives/Move.bat
@@ -0,0 +1,3 @@
+cd C:\Users\Zac\Desktop\Ragin's Sets\AMERPL
+move AMERPL.grf C:\Users\Zac\Documents\OpenTTD\newgrf
+pause
